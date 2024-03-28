5cbd616253a86b65110b18098c7ca5b9eaf15691
smessie <smessie@smessie.com>
2024-03-28 10:00:08 +0100

    Cleanup code

--- bin/dumpsToFeed.ts
+++ bin/dumpsToFeed.ts
@@ -1,6 +1,5 @@
-const { program } = require('commander');
-import {Level} from "level";
-import { main } from "../index";
+import {program} from 'commander';
+import {main} from "../index";
 
 program
   .name('dumps-to-feed')
@@ -11,13 +10,8 @@ program.argument('<feedname>', 'name of the feed you want to update')
   .argument('<filename>', 'filename of the dump')
   .option('-f, --flush') //TODO: add parameters for setting the shapes and strategy to extract focus nodes.
   .action(async (feedname: string, filename: string, options: any) => {
-    const db = new Level("state-of-" + feedname, { valueEncoding: 'json' })
-    if (options.flush) {
-        //Check for the flag --reset. If set, flush the db first
-        await db.clear();
-    }
     //TODO: add the other parameters
-    main(db, feedname, filename, []);
+    await main(feedname, options.flush, filename, []);
   });
 
 program.parse();
