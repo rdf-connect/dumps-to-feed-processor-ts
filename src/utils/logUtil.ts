9fd6dc9b79f50b8f0a67ef95c3fad23bc8300731
smessie <smessie@smessie.com>
2024-10-21 10:39:12 +0200

    fix: Properly reuse loggers to resolve memory issues

--- src/utils/logUtil.ts
+++ src/utils/logUtil.ts
@@ -1,66 +1,71 @@
-import winston, { format, Logger } from "winston";
+import winston, {format, Logger} from "winston";
 
 const PROCESSOR_NAME = "dumps-to-feed-processor-ts";
 
 const consoleTransport = new winston.transports.Console();
 consoleTransport.level =
-    process.env.LOG_LEVEL ||
+   process.env.LOG_LEVEL ||
    (process.env.DEBUG?.includes(PROCESSOR_NAME) || process.env.DEBUG === "*"
-        ? "debug"
-        : "info");
+      ? "debug"
+      : "info");
 
-const classLoggers = new WeakMap<Instance, Logger>();
+const classLoggers = new WeakMap<Constructor, Logger>();
+const stringLoggers = new Map<string, Logger>();
 
 export function getLoggerFor(loggable: string | Instance): Logger {
-    let logger: Logger;
-    if (typeof loggable === "string") {
-        logger = createLogger(loggable);
-    } else {
-        const { constructor } = loggable;
-        if (classLoggers.has(loggable)) {
-            logger = classLoggers.get(loggable)!;
-        } else {
-            logger = createLogger(constructor.name);
-            classLoggers.set(loggable, logger);
-        }
-    }
-    return logger;
+   let logger: Logger;
+   if (typeof loggable === "string") {
+      if (stringLoggers.has(loggable)) {
+         logger = stringLoggers.get(loggable)!;
+      } else {
+         logger = createLogger(loggable);
+         stringLoggers.set(loggable, logger);
+      }
+   } else {
+      const {constructor} = loggable;
+      if (classLoggers.has(constructor)) {
+         logger = classLoggers.get(constructor)!;
+      } else {
+         logger = createLogger(constructor.name);
+         classLoggers.set(constructor, logger);
+      }
+   }
+   return logger;
 }
 
 function createLogger(label: string): Logger {
-    return winston.createLogger({
-        format: format.combine(
-            format.label({ label }),
-            format.colorize(),
-            format.timestamp(),
-            format.metadata({
-                fillExcept: ["level", "timestamp", "label", "message"],
-            }),
-            format.printf(
-                ({
-                    level: levelInner,
-                    message,
-                    label: labelInner,
-                    timestamp,
-                    metadata: meta,
-                }): string =>
-                    `${timestamp} {${PROCESSOR_NAME}} [${labelInner}] ${levelInner}: ${message}`,
-            ),
-        ),
-        transports: [consoleTransport],
-    });
+   return winston.createLogger({
+      format: format.combine(
+         format.label({label}),
+         format.colorize(),
+         format.timestamp(),
+         format.metadata({
+            fillExcept: ["level", "timestamp", "label", "message"],
+         }),
+         format.printf(
+            ({
+                level: levelInner,
+                message,
+                label: labelInner,
+                timestamp,
+             }): string =>
+               `${timestamp} {${PROCESSOR_NAME}} [${labelInner}] ${levelInner}: ${message}`,
+         ),
+      ),
+      transports: [consoleTransport],
+   });
 }
 
 /**
  * Any class constructor.
  */
 interface Constructor {
-    name: string;
+   name: string;
 }
 
 /**
  * Any class instance.
  */
 interface Instance {
-    constructor: Constructor;
+   constructor: Constructor;
 }
